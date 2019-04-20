334d4d23071231099e7a629ffc088bf715f0c922
ttsia <ttsia@softserveinc.com>
2019-04-20 15:40:37 +0300

    Ticket: Migration

--- migrations/movie_last_updated_migration.py
+++ migrations/movie_last_updated_migration.py
@@ -1,29 +1,24 @@
+import configparser
+import os
+
+import dateutil.parser as parser
+from bson import ObjectId
 from pymongo import MongoClient, UpdateOne
 from pymongo.errors import InvalidOperation
-from bson import ObjectId
-import dateutil.parser as parser
 
 """
-Ticket: Migration
-
 Update all the documents in the `movies` collection, such that the "lastupdated"
 field is stored as an ISODate() rather than a string.
-
-The parser.parse() method can transform date strings into ISODate objects for
-us. We just need to make sure the correct operations are sent to MongoDB!
 """
+config = configparser.ConfigParser()
+# meh...
+config.read(os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../.ini')))
 
-# ensure you update your host information below!
-host = "mongodb://localhost:27017"
+host = config['PROD']['MFLIX_DB_URI']
 mflix = MongoClient(host)["mflix"]
 
-# TODO: Create the proper predicate and projection
-# add a predicate that checks that the "lastupdated" field exists, and then
-# checks that its type is a string
-# a projection is not required, but may help reduce the amount of data sent
-# over the wire!
-predicate = {"some_field": {"$some_operator": "some_expression"}}
-projection = None
+predicate = {"lastupdated": {"$type": "string", "$exists": "true"}}
+projection = {"lastupdated": 1}
 
 cursor = mflix.movies.find(predicate, projection)
 
@@ -42,12 +37,15 @@ for doc in cursor:
 print(f"{len(movies_to_migrate)} documents to migrate")
 
 try:
-    # TODO: Complete the UpdateOne statement below
-    # build the UpdateOne so it updates the "lastupdated" field to contain
-    # the new ISODate() type
     bulk_updates = [UpdateOne(
-        {"_id": movie.get("doc_id")},
-        {"$some_update_operator": {"some_field_to_update"}}
+        {
+            "_id": movie.get("doc_id")
+        },
+        {
+            "$set": {
+                "lastupdated": movie.get("lastupdated")
+            }
+        }
     ) for movie in movies_to_migrate]
 
     # here's where the bulk operation is sent to MongoDB
